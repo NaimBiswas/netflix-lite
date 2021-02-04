18862073cd2efb63b57802197c2633413fd441c6
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-04 14:47:06 +0600

    create some row and set some value

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import NavMenu from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Banner from './components/Banner';
-
+import request from './Request'
+import Row from './components/Row';
 
 function App() {
    return (
@@ -14,6 +15,14 @@ function App() {
             {/* baner  */}
             <Banner></Banner>
             {/* Home  */}
+            <Row title='Treanding Now' fetchUrl={request.fetchTrending}></Row>
+            <Row title='Top Rated Movies' fetchUrl={request.fetchTopRated}></Row>
+            <Row title='Action Movies' fetchUrl={request.fetchActionMovies}></Row>
+            <Row title='Comedy Movies' fetchUrl={request.fetchComedyMovies}></Row>
+            <Row title='Horrom Movies' fetchUrl={request.fetchHorrorMovies}></Row>
+            <Row title='Romactic Movie' fetchUrl={request.fetchRomanticMovies}></Row>
+            <Row title='Coumentaries' fetchUrl={request.fetchDocumertaries}></Row>
+
          </div>
       </Fragment>
 
