e0358617b3327ddf48e35f304268838b63ce511c
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-04 11:22:55 +0600

    done work with nav bar and set it sticky

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Fragment } from 'react';
 import './App.css';
-import Logo from './Logo.svg'
+import NavMenu from './components/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
    return (
       <Fragment>
          <div className="App">
-
+            {/* nav  */}
+            <NavMenu></NavMenu>
+            {/* Home  */}
          </div>
       </Fragment>
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { Button, Form, FormControl, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import Logo from '../Logo.svg'
+import '../componentsCSS/Nav.css'
+const NavMenu = () => {
+   const [show, handleShow] = useState(false);
+   const transitionNavBar = () => {
+      if (window.scrollY > 100) {
+         handleShow(true);
 
-const Nav = () => {
+      } else {
+         handleShow(false);
+      }
+
+   }
+   useEffect(() => {
+      window.addEventListener("scroll", transitionNavBar);
+      return () => window.addEventListener("scroll", transitionNavBar)
+   }, [])
    return (
-      <div>
-
-      </div>
+      <Fragment>
+         <div className="header">
+            <Navbar className={`nav ${show && 'nav_bg'}`} expand="lg">
+               <Navbar.Brand href="#home"><img src={Logo} alt="" /></Navbar.Brand>
+               <Navbar.Toggle aria-controls="basic-navbar-nav" />
+               <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="mr-auto">
+                     <Nav.Link className='text-white' href="#home">Home</Nav.Link>
+                     <Nav.Link className='text-white' href="#link">Tv Shows</Nav.Link>
+                     <Nav.Link className=' text-white' href="#link">Movies</Nav.Link>
+                     <Nav.Link className='text-white' href="#link">Recently Added</Nav.Link>
+                     <Nav.Link className='text-white' href="#link">My List</Nav.Link>
+                  </Nav>
+                  <Nav>
+                     <Nav.Link className='text-white'> KID</Nav.Link>
+                     <Nav.Link className='text-white'> DVD</Nav.Link>
+                     <img className='right-avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+                  </Nav>
+               </Navbar.Collapse>
+            </Navbar>
+         </div>
+      </Fragment >
    )
 }
 
-export default Nav
+export default NavMenu
