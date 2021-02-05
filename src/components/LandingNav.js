07018c7f25ef1cd6ad94128da1ac8376b31b3ff2
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-05 10:41:40 +0600

    set nav logo

--- src/components/LandingNav.js
+++ src/components/LandingNav.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, { Fragment } from 'react'
+import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
+import Logo from '../Logo.svg'
 const LandingNav = () => {
    return (
-      <div>
-         <h2 className="text-white">This is your landing Page Nav Bar</h2>
-      </div>
+      <Fragment>
+         <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home"><img src={Logo} alt="" /></Navbar.Brand>
+            <Nav className="mr-auto">
+            </Nav>
+            <Form inline>
+               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+               <Button variant="outline-info">Search</Button>
+            </Form>
+         </Navbar>
+      </Fragment>
    )
 }
 
