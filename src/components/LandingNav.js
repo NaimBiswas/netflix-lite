f32b80c25ac55d3e2bf0a27293b26937bb4798ec
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-05 12:58:06 +0600

    set a login route

--- src/components/LandingNav.js
+++ src/components/LandingNav.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { Button, Nav, Navbar } from 'react-bootstrap'
 import Logo from '../Logo.svg'
 import '../componentsCSS/LandingNav.css'
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom'
 const LandingNav = () => {
    return (
       <Fragment>
@@ -10,7 +11,11 @@ const LandingNav = () => {
                <Navbar.Brand href="#home"><img className='Logo_header' src={Logo} alt="" /></Navbar.Brand>
                <Nav className="mr-auto">
                </Nav>
-               <Button className='Landing_button'>Sing In</Button>
+               <Router>
+                  <Route>
+                     <Link to='/login'><Button className='Landing_button'> Sing In </Button></Link>
+                  </Route>
+               </Router>
             </Navbar>
          </div>
 
