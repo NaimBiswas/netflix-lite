a78f7f36bcdbf59a9cbcf2cb25d687ec76151e6a
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-05 12:24:24 +0600

    video successfully palying

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -24,7 +24,7 @@ const LandingPage = () => {
                            <img alt="" className="our-story-card-img" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" data-uia="our-story-card-img" />
                            <div className="our-story-card-animation">
 
-                              <video class="our-story-card-video" autoplay="" playsinline="" muted="" loop="">
+                              <video className="our-story-card-video" autoPlay playsInline muted loop>
                                  <source src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" type="video/mp4" />
                               </video>
                               <div className="our-story-card-animation-text">
