e3d593f8ae5c6219ca9420fb56e43df750a6bd88
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-05 22:24:46 +0600

    install react-redux

--- src/components/userSlice.js
+++ src/components/userSlice.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userReduce'
 export default configureStore({
    reuducer: {
-      user: userReducer,
+      user: userReducer
    }
 
 })
