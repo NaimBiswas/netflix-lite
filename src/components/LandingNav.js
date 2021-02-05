decbc88fe9594f1a85c62ecd3bc0dda0c886de3b
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-05 10:46:59 +0600

    set padding and logo size

--- src/components/LandingNav.js
+++ src/components/LandingNav.js
@@ -1,18 +1,20 @@
 import React, { Fragment } from 'react'
-import { Button, Form, FormControl, Nav, Navbar } from 'react-bootstrap'
+import { Button, Nav, Navbar } from 'react-bootstrap'
 import Logo from '../Logo.svg'
+import '../componentsCSS/LandingNav.css'
 const LandingNav = () => {
    return (
       <Fragment>
-         <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home"><img src={Logo} alt="" /></Navbar.Brand>
-            <Nav className="mr-auto">
-            </Nav>
-            <Form inline>
-               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-               <Button variant="outline-info">Search</Button>
-            </Form>
-         </Navbar>
+         <div className="container_nav">
+            <Navbar variant="dark">
+               <Navbar.Brand href="#home"><img className='Logo_header' src={Logo} alt="" /></Navbar.Brand>
+               <Nav className="mr-auto">
+               </Nav>
+               <Button>Sing In</Button>
+            </Navbar>
+
+         </div>
+
       </Fragment>
    )
 }
