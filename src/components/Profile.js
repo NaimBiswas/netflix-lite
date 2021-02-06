11fcaa461589a00e662812075f265627e7203e6a
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-06 13:53:31 +0600

    set a backgorund

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,7 +12,16 @@ const Profile = () => {
    return (
       <div>
          <Nav></Nav>
-         <div style={{ paddingTop: '150px' }} className="container">
+         <div style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'top center',
+            width: '100%',
+            height: '100vh',
+            backgroundImage: `linear-gradient(0deg, rgb(197 25 25 / 30%), rgb(0 0 0)),url("https://assets.nflxext.com/ffe/siteui/vlv3/538717c0-f271-40e9-a99e-b07f9c521322/1b4054cd-e5f5-40a8-88d3-09428654d5ff/BD-en-20210201-popsignuptwoweeks-perspective_alpha_website_medium.jpg")`,
+            position: 'relative'
+         }}>
+         </div>
+         <div style={{ position: "absolute", top: '18%', right: '0' }} className="container">
             <div className="row">
                <div className="col-lg-6 offset-lg-2">
                   <table className='table'>
